d4735121a4bf52ba1ee567e64fda6b6b39e10f79
ykocar <77811733+ykocar@users.noreply.github.com>
2026-09-02 11:29:44 +0100

    AES-723: Add automated API test flow for IE507A cancellation journey

---
+++ src/test/scala/uk/gov/hmrc/api/specs/CancelSubmissionSpec.scala
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.api.specs
+
+import org.scalatest.BeforeAndAfterAll
+import uk.gov.hmrc.api.helpers.PayloadLoader
+
+import java.util.UUID
+
+class CancelSubmissionSpec extends BaseSpec with BeforeAndAfterAll {
+
+  private var bearerToken: String  = _
+  private var submissionId: String = _
+
+  override def beforeAll(): Unit = {
+    bearerToken = service.getBearerToken.futureValue
+
+    val xml =
+      PayloadLoader.load("valid-ie507.xml")
+
+    service
+      .submitMessage(
+        xml,
+        bearerToken
+      )
+      .futureValue
+
+    val submissionsResponse =
+      service
+        .getSubmissions(bearerToken)
+        .futureValue
+
+    submissionId = "<submissionId>(.*?)</submissionId>".r
+      .findFirstMatchIn(submissionsResponse.body)
+      .map(_.group(1))
+      .getOrElse(
+        throw new RuntimeException(
+          "No submissionId found in submissions list - cannot proceed with test setup"
+        )
+      )
+
+    println(s"[DEBUG] beforeAll -> using submissionId: $submissionId")
+    println(s"[DEBUG] beforeAll -> raw getSubmissions body: ${submissionsResponse.body}")
+  }
+
+  Feature("Cancel Submission by SubmissionId") {
+
+    Scenario("Authenticated user can cancel a submission using a valid submissionId") {
+
+      Given("a valid bearer token and a submissionId belonging to the authenticated user")
+
+      When("the cancel endpoint is called with that submissionId")
+
+      val response =
+        service
+          .cancelSubmission(submissionId, bearerToken)
+          .futureValue
+
+      println(s"[DEBUG] cancelSubmission -> status: ${response.status}, body: '${response.body}'")
+
+      Then("a no content response is returned")
+
+      response.status shouldBe 204
+
+      And("the submission can be retrieved successfully after cancellation")
+
+      val getResponse =
+        service
+          .getSubmission(submissionId, bearerToken)
+          .futureValue
+
+      println(s"[DEBUG] getSubmission after cancel -> status: ${getResponse.status}")
+      println(s"[DEBUG] getSubmission after cancel -> body: ${getResponse.body}")
+
+      getResponse.status shouldBe 200
+
+      And("the retrieved submission reflects the cancelled status")
+
+      getResponse.body should include("<submissionId>" + submissionId + "</submissionId>")
+      getResponse.body should include("<status>3</status>")
+    }
+
+    Scenario("Cancelling an already cancelled submission is idempotent") {
+
+      Given("a submissionId that has already been cancelled")
+
+      When("the cancel endpoint is called again with that submissionId")
+
+      val response =
+        service
+          .cancelSubmission(submissionId, bearerToken)
+          .futureValue
+
+      println(s"[DEBUG] cancelSubmission (2nd call) -> status: ${response.status}, body: '${response.body}'")
+
+      Then("a no content response is still returned")
+
+      response.status shouldBe 204
+    }
+
+    Scenario("Cancelling a submission with a non-existent submissionId returns 404") {
+
+      Given("a valid bearer token")
+
+      And("a submissionId that does not exist")
+
+      val nonExistentId =
+        UUID.randomUUID().toString
+
+      When("the cancel endpoint is called with that submissionId")
+
+      val response =
+        service
+          .cancelSubmission(nonExistentId, bearerToken)
+          .futureValue
+
+      println(s"[DEBUG] cancelSubmission (non-existent id: $nonExistentId) -> status: ${response.status}")
+
+      Then("a not found response is returned")
+
+      response.status shouldBe 404
+    }
+
+    Scenario("Users cannot cancel submissions belonging to another EORI") {
+
+      Given("a bearer token for a different EORI")
+
+      import uk.gov.hmrc.api.models.{AuthStubEnrolment, AuthStubIdentifier, AuthStubRequest}
+
+      val otherUserToken =
+        service
+          .getBearerToken(
+            AuthStubRequest(
+              enrolments = Seq(
+                AuthStubEnrolment(
+                  key = "HMRC-CUS-ORG",
+                  identifiers = Seq(
+                    AuthStubIdentifier(
+                      key = "EORINumber",
+                      value = "GB99999999"
+                    )
+                  )
+                )
+              )
+            )
+          )
+          .futureValue
+
+      When("that user attempts to cancel a submission belonging to another EORI")
+
+      val response =
+        service
+          .cancelSubmission(submissionId, otherUserToken)
+          .futureValue
+
+      println(s"[DEBUG] cancelSubmission (other EORI token, submissionId: $submissionId) -> status: ${response.status}")
+
+      Then("a not found response is returned, without revealing the submission exists")
+
+      response.status shouldBe 404
+    }
+  }
+}
